01f3e9242833f8ee68325a539c8f79c5a8030352
Adam Singer <financeCoding@gmail.com>
2015-04-07 20:13:09 -0700

    Adding dart tests based on the test input files.

---
+++ dart/tests/all.dart
@@ -0,0 +1,11 @@
+library all_tests;
+
+import "step0_repl_test.dart" as step0;
+import "step1_read_print_test.dart" as step1;
+import "step2_eval_test.dart" as step2;
+
+void main() {
+  step0.main();
+  step1.main();
+  step2.main();
+}
---
+++ dart/tests/test_helpers.dart
@@ -0,0 +1,25 @@
+library test_helpers;
+
+import "../types.dart";
+
+typedef MalType TestEval(String input);
+
+void expectResults(TestEval testEval, String input, String expectedOutput, {isFailing: false}) {
+  String actualOutput;
+
+  try {
+    actualOutput = testEval(input).toString();
+  } on StateError catch (se) {
+    actualOutput = se.message;
+  }
+
+  if (expectedOutput == actualOutput) {
+    print("PASS: testEval('$input') => ${expectedOutput} == ${actualOutput}");
+  } else {
+    print("FAILED: testEval('$input') => ${expectedOutput} != ${actualOutput}");
+  }
+
+  if (!isFailing) {
+    assert(expectedOutput == actualOutput);
+  }
+}
